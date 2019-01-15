40f8df97c4c6246e49c321fdd9f603aab0f9fd29
FranciscoComasGomez <franciscocomas@iespaucasesnoves.cat>
2019-01-15 18:54:16 +0100

    Insert de vi dona null pointerexception

--- app/src/main/java/com/example/xisco/gitprova/AfegirVi.java
+++ app/src/main/java/com/example/xisco/gitprova/AfegirVi.java
@@ -2,12 +2,44 @@ package com.example.xisco.gitprova;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class AfegirVi extends AppCompatActivity {
-
+public class AfegirVi extends AppCompatActivity implements View.OnClickListener{
+    private Button afg;
+    private DataSourceVi data;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_afegir_vi);
+        data = new DataSourceVi(AfegirVi.this);
+        afg = findViewById(R.id.inserir);
+        afg.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View v) {
+
+        if(v == afg){
+            Toast.makeText(this, "boton", Toast.LENGTH_SHORT).show();
+            Vi vi = new Vi();
+            vi.setId(3);
+            vi.setFoto("asd");
+            vi.setNomVi(findViewById(R.id.afgNomVi).toString());
+            vi.setAnada(findViewById(R.id.afgAnada).toString());
+            vi.setComentari(findViewById(R.id.afgCom).toString());
+            vi.setData(findViewById(R.id.afgData).toString());
+            vi.setGraduacio(findViewById(R.id.afgGra).toString());
+            vi.setIdDenominacio(2);
+            vi.setLloc(findViewById(R.id.afgLloc).toString());
+            vi.setNota(Integer.valueOf(R.id.afgNota));
+            vi.setPreu(Integer.valueOf(R.id.afgPreu));
+            vi.setTipus(findViewById(R.id.afgTipus).toString());
+            vi.setValGustativa(findViewById(R.id.afgValGust).toString());
+            vi.setValOlfativa(findViewById(R.id.afgValOlf).toString());
+            vi.setValVisual(findViewById(R.id.afgValVisual).toString());
+            data.createVi(vi);
+        }
     }
 }
--- app/src/main/java/com/example/xisco/gitprova/EditaVi.java
+++ app/src/main/java/com/example/xisco/gitprova/EditaVi.java
@@ -2,12 +2,15 @@ package com.example.xisco.gitprova;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class EditaVi extends AppCompatActivity {
-
+    private EditText nomvi;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edita_vi);
+        nomvi = findViewById(R.id.nomVi);
+        nomvi.setText("asd");
     }
 }
